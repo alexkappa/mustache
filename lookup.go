fda9de949a5a7ef65017bfbd252882093044e9a3
alexkappa <alex.kalyvitis@gmail.com>
2014-12-08 13:21:16 +0100

    credit where credit is due

--- lookup.go
+++ lookup.go
@@ -1,4 +1,5 @@
 // Copyright (c) 2014 Alex Kalyvitis
+// Portions Copyright (c) 2009 Michael Hoisie
 
 package mustache
 
@@ -8,11 +9,13 @@ import (
 )
 
 // The lookup function searches for a property that matches name within the
-// context chain.
+// context chain. We first start from the first item in the context chain which
+// is the most likely to have the value we're looking for. If not found, we'll
+// move up the chain and repeat.
 func lookup(name string, context ...interface{}) (interface{}, bool) {
 	// If the dot notation was used we split the word in two and perform two
 	// consecutive lookups. If the first one fails we return no value and a
-	// negative truth.
+	// negative truth. Taken from github.com/hoisie/mustache.
 	if name != "." && strings.Contains(name, ".") {
 		parts := strings.SplitN(name, ".", 2)
 		if value, ok := lookup(parts[0], context...); ok {
@@ -22,16 +25,24 @@ func lookup(name string, context ...interface{}) (interface{}, bool) {
 	}
 	// Iterate over the context chain and try to match the name to a value.
 	for _, c := range context {
+		// Reflect on the value of the current context.
 		reflectValue := reflect.ValueOf(c)
+		// If the name is ".", we should return the whole context as-is.
 		if name == "." {
 			return c, truth(reflectValue)
 		}
 		switch reflectValue.Kind() {
+		// If the current context is a map, we'll look for a key in that map
+		// that matches the name.
 		case reflect.Map:
 			mapValue := reflectValue.MapIndex(reflect.ValueOf(name))
 			if mapValue.IsValid() {
 				return mapValue.Interface(), truth(mapValue)
 			}
+		// If the current context is a struct, we'll look for a property in that
+		// struct that matches the name. In the near future I'd like to add
+		// support for matching struct names to tags so we can use lower_case
+		// names in our templates which makes it more mustache like.
 		case reflect.Struct:
 			fieldValue := reflectValue.FieldByName(name)
 			if fieldValue.IsValid() {
@@ -44,10 +55,20 @@ func lookup(name string, context ...interface{}) (interface{}, bool) {
 				}
 			}
 		}
+		// If by this point no value was matched, we'll move up a step in the
+		// chain and try to match a value there.
 	}
+	// We've exhausted the whole context chain and found nothing. Return a nil
+	// value and a negative truth.
 	return nil, false
 }
 
+// The truth function will tell us if r is a truthy value or not. This is
+// important for sections as they will render their content based on the output
+// of this function.
+//
+// Zero values are considered falsy. For example an empty string, the integer 0
+// and so on are all considered falsy.
 func truth(r reflect.Value) bool {
 	switch r.Kind() {
 	case reflect.Array, reflect.Slice:
